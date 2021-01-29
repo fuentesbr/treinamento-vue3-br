213cb3d413a268b4a6d100035dad736ed0a37721
Cesar Fuentes <cesar@fuentes.net.br>
2021-01-29 19:53:20 -0300

    inicio do projeto

---
+++ data-binding/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1 :style="{ textDecoration: decoration }">Hello {{ name }}!</h1>
+        <input type="text" v-model="name"> 
+        <br>
+        <br>
+        <a :href="link">Link pro curso!</a>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: 'Cesar',
+        decoration: 'underline',
+        link: 'https://fuentes.net.br'
+    })
+}
+</script>
+
+<style>
+
+</style>
---
+++ lifecycle-hooks/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Lifecycle Hooks</h1>
+</template>
+
+<script>
+import { onMounted } from 'vue'
+
+export default {
+    setup() {
+        onMounted(() => {
+            console.log('Foi montado composition API')
+        })
+    }
+}
+/*
+export default {
+    mounted () {
+        console.log('Foi montado options API')
+    }
+}*/
+</script>
+
+<style>
+
+</style>
